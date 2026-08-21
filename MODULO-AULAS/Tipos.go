7611ad64c821819589d6381207ee37027bae76b1
Cauã Melo <cauamcz06@gmail.com>
2026-08-21 12:35:35 -0300

    Add files via upload
    alguns testes feitos , varconst incompleto

---
+++ MODULO-AULAS/Tipos.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	//tipos:
+	//boll(true,false)
+	fmt.Printf("Type:%T-Valure:%v\n", true, true)
+
+	//string-sequencia bytes
+	fmt.Printf("Type:%T-Valure:%v\n", "setph", "setph")
+	fmt.Printf("Type:%T-Valure:%v\n", "1", "1")
+
+	//int
+	fmt.Printf("Type:%T-Valure:%v\n", 1, 1)
+
+	//float(float64,float32)decimal
+	fmt.Printf("Type:%T-Valure:%v\n", 1.233, 1.233)
+}
---
+++ MODULO-AULAS/varconst.go
@@ -0,0 +1,12 @@
+package main
+
+func main() {
+	//variaveis
+	//var+nome da variavel+tipo
+	var nome string
+	nome = "bento"
+	fmt.println(nome)
+
+	nome = "steph"
+	fmt.println(nome)
+}
